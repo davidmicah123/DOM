426323b06ca1477d4ec8874094d13310752e997f
davidmicah123 <micahdanielsd@gmail.com>
2022-04-11 18:23:32 +0100

    Working with Arrays

---
+++ script.js
@@ -0,0 +1,13 @@
+var icons = document.getElementsByClassName('fa');
+
+
+// for (var i = 0; i < icons.length; i++){
+//     console.log(icons);
+// }
+
+
+// var iconArr = Array.from(icons);
+// console.log(iconArr);
+// console.log(icons);
+
+// iconArr.push("helo");
