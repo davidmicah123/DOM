18c1a1e93446c585e8a3f8af789b2cab2159def4
davidmicah123 <micahdanielsd@gmail.com>
2022-04-13 20:16:33 +0100

    Working on the Note functionality

--- script.js
+++ script.js
@@ -1,32 +1,93 @@
-var icons = document.getElementsByClassName('fa');
+// var newEl = document.createElement('button');
 
 
-// for (var i = 0; i < icons.length; i++){
-//     console.log(icons);
-// }
+// var text = document.createTextNode("Click");
+
+// newEl.appendChild(text);
+// newEl.setAttribute('style', 'display: block; background-color: blueviolet; color: white; font-weight: bolder; border: 0; padding: 1vh; border-raddius: 0.5em;')
+// console.log(newEl);
+
+// var form = document.getElementById('add');
+
+// form.appendChild(newEl);
 
 
-// var iconArr = Array.from(icons);
-// console.log(iconArr);
-// console.log(icons);
+// form.insertBefore(newEl, form.children[0]);
 
-// iconArr.push("helo");
-// // console.log(iconArr)
+// newEl.parentElement.removeChild(newEl)
 
-// Array.from(icons).forEach(function(icon){
-//     console.log(icon)
-// });
+var ul = document.querySelector('ul');
+document.getElementById('add-btn').addEventListener('click', function(e){
+    e.preventDefault();
+    var addinput = document.getElementById('add-input');
 
 
+    if(addinput.value !== ''){
+        
+        var li = document.createElement('li'),
+            firstper = document.createElement('p'),
+            secondper = document.createElement('p'),
+            firsticon = document.createElement('i'),
+            secondicon = document.createElement('i'),
+            input = document.createElement('input');
+    
+        firsticon.className = "fa fas fa-lock";
+        secondicon.className = "fa fa-times"
+        input.className = "edit-note"
+        input.setAttribute('type', 'text');
+    
+        firstper.innerHTML = addinput.value;
+    
+        secondper.appendChild(firsticon);
+        secondper.appendChild(secondicon);
+    
+        li.appendChild(firstper);
+        li.appendChild(secondper);
+        li.appendChild(input);
+        ul.appendChild(li);
+    
+        addinput.value = '';
+    }
 
-let lis = document.getElementsByTagName('li');
+});
 
-console.log(lis);
 
-let divs =  document.querySelectorAll("div, li, #hide-list ");
+//Hide Items
 
-console.log(divs);
+var hideitems = document.getElementById('hide');
 
-// divs.forEach(function(div){
-//     console.log(div);
-// });
+hideitems.addEventListener('click', function(e){
+
+    var label = document.querySelector('label');
+
+    if(hideitems.checked){
+        label.textContent = 'unhide notes';
+        ul.style.display = 'none';
+    }else{
+        label.textContent = 'hidenotes';
+        ul.style.display = 'block';
+    }
+});
+
+
+//Search filter
+
+var searchinput = document.querySelector('#search-note input');
+
+searchinput.addEventListener('keyup', function(e) {
+    var searchchar = e.target.value.toUpperCase();
+    // console.log(searchchar);
+
+    var notes = ul.getElementsByTagName('li');
+
+    Array.from(notes).forEach(function(note){
+        var parText = note.firstElementChild.innerHTML;
+ 
+        if (parText.toUpperCase().indexOf(searchchar) !== -1){
+            note.style.display = 'block';
+        }else{
+            note.style.display = 'none';
+        }
+
+    });
+});
